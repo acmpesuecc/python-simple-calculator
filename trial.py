c5a4aafd08237d3c72ce17cd6d381263ceebddf3
Tharun Kumar <tharunkp41@gmail.com>
2022-10-21 22:33:20 +0530

    color changed

--- trial.py
+++ trial.py
@@ -1,4 +1,4 @@
-# importing all the libraries we are making use of (tkfloater and custom MyMath module
+# importing all the libraries we are making use of (tkinter and custom MyMath module
 
 from tkinter import *
 from MyMath import *
@@ -18,24 +18,24 @@ def area():
     SelectedShape = shapes.get()
 
     if SelectedShape == "Rectangle":
-        l = float(radius_entry.get())
-        b = float(height_entry.get())
+        l = int(radius_entry.get())
+        b = int(height_entry.get())
         area_rectangle = myArea(SelectedShape.lower(), l, b, 0, 0)
         messagebox.showinfo("Area Rectangle", area_rectangle)
 
     elif SelectedShape == "Square":
-        r = float(radius_entry.get())
+        r = int(radius_entry.get())
         area_square = myArea(SelectedShape.lower(), r, 0, 0, 0)
         messagebox.showinfo("Area Square", area_square)
 
     elif SelectedShape == "Triangle":
-        b = float(radius_entry.get())
-        h = float(height_entry.get())
+        b = int(radius_entry.get())
+        h = int(height_entry.get())
         area_triangle = myArea(SelectedShape.lower(), 0, b, h, 0)
         messagebox.showinfo("Area Triangle", area_triangle)
 
     elif SelectedShape == "Circle":
-        r = float(radius_entry.get())
+        r = int(radius_entry.get())
         area_circle = myArea(SelectedShape.lower(), 0, 0, 0, r)
         messagebox.showinfo("Area Circle", area_circle)
     else:
@@ -47,29 +47,29 @@ def area():
 def volume():
     selected_solid_shape = solidshape.get()
     if selected_solid_shape == "Sphere":
-        r = float(radius_entry2.get())
+        r = int(radius_entry2.get())
         vol_sphere = myvolume(selected_solid_shape.lower(), 0, 0, 0, r)
         messagebox.showinfo("Volume Sphere", vol_sphere)
     elif selected_solid_shape == "Cone":
-        r = float(radius_entry2.get())
-        h = float(height2.get())
+        r = int(radius_entry2.get())
+        h = int(height2.get())
         vol_cone = myvolume(selected_solid_shape.lower(), 0, 0, h, r)
         messagebox.showinfo("Volume Cone", vol_cone)
 
     elif selected_solid_shape == "Cube":
-        l = float(radius_entry2.get())
+        l = int(radius_entry2.get())
         vol_cube = myvolume(selected_solid_shape.lower(), l, 0, 0, 0)
         messagebox.showinfo("Volume Cube", vol_cube)
     elif selected_solid_shape == "Cylinder":
-        r = float(radius_entry2.get())
-        h = float(height2.get())
+        r = int(radius_entry2.get())
+        h = int(height2.get())
         vol_cylinder = myvolume(selected_solid_shape.lower(), 0, 0, h, r)
         messagebox.showinfo("Volume Cylinder", vol_cylinder)
 
     elif selected_solid_shape == "Cuboid":
-        l = float(radius_entry2.get())
-        b = float(width_entry.get())
-        h = float(height2.get())
+        l = int(radius_entry2.get())
+        b = int(width_entry.get())
+        h = int(height2.get())
         vol_cuboid = myvolume(selected_solid_shape.lower(), l, b, h, 0)
         messagebox.showinfo("Volume Cuboid", vol_cuboid)
 
@@ -82,9 +82,9 @@ def volume():
 def condition_check():
     tocheck = conditions.get()
     if tocheck == "Pythagorean Triplet Checker":
-        a = float(side1_entry.get())
-        b = float(side2_entry.get())
-        c = float(side3_entry.get())
+        a = int(side1_entry.get())
+        b = int(side2_entry.get())
+        c = int(side3_entry.get())
         py_check = mycondition(tocheck, a, b, c, 0)
         if py_check == 1:
             messagebox.showinfo("Check", "This is a Pythagorean Triplet.")
@@ -92,7 +92,7 @@ def condition_check():
             messagebox.showinfo("Check", "This is not a Pythagorean Triplet.")
 
     elif tocheck == "Complimentary&Supplementary Angles":
-        angle = float(angle_entry.get())
+        angle = int(angle_entry.get())
         comsup = mycondition(tocheck, 0, 0, 0, angle)
 
 
@@ -101,9 +101,9 @@ note1 = ttk.Notebook(root)
 note1.pack(pady=5)
 
 # creating 3 frames
-area_frame = Frame(note1, width=300, height=300 , )
-volume_frame = Frame(note1, width=300, height=300)
-condition_frame = Frame(note1, width=300, height=300)
+area_frame = Frame(note1, width=300, height=300,bg='sky blue')
+volume_frame = Frame(note1, width=300, height=300,bg='sky blue')
+condition_frame = Frame(note1, width=300, height=300,bg='sky blue')
 area_frame.pack(fill="both", expand=1)
 volume_frame.pack(fill="both", expand=1)
 condition_frame.pack(fill="both", expand=1)
@@ -137,51 +137,50 @@ def show3():
 # area frame option
 shape_options = OptionMenu(
     area_frame, shapes, "Circle", "Square", "Triangle", "Rectangle").pack()
-mybutton1 = Button(area_frame, text="Select shape", command=show1, bg='red').pack()
+mybutton1 = Button(area_frame, text="Select shape", command=show1,fg='yellow',bg='red').pack()
 
 radius_side = Label(area_frame, text="Enter radius or side in m").pack()
-
-radius_entry = Entry(area_frame, font=("Helvetica", 20))
+radius_entry = Entry(area_frame, font=("Helvetica", 20),bg='blue')
 radius_entry.pack()
 
 height = Label(
     area_frame, text="Enter height or width in m if applicable else enter 0").pack()
-height_entry = Entry(area_frame, font=("Helvetica", 20))
+height_entry = Entry(area_frame, font=("Helvetica", 20),bg='blue')
 height_entry.pack()
 
 
 # volume frame option
 volume_options = OptionMenu(
     volume_frame, solidshape, "Cone", "Sphere", "Cylinder", "Cube", "Cuboid").pack()
-mybutton2 = Button(volume_frame, text="Select shape", command=show2 , bg='blue' ).pack()
+mybutton2 = Button(volume_frame, text="Select shape", command=show2,fg='yellow',bg='red').pack()
 radius_side2 = Label(volume_frame, text="Enter radius or side in m").pack()
-radius_entry2 = Entry(volume_frame, font=("Helvetica", 20))
+radius_entry2 = Entry(volume_frame, font=("Helvetica", 20),bg='blue')
 radius_entry2.pack()
 height2 = Label(
     volume_frame, text="Enter height in m if applicable else enter 0").pack()
-height2 = Entry(volume_frame, font=("Helvetica", 20))
+height2 = Entry(volume_frame, font=("Helvetica", 20),bg='blue')
 height2.pack()
 width = Label(
     volume_frame, text="Enter breadth in m if applicable else enter 0").pack()
-width_entry = Entry(volume_frame, font=("Helvetica", 20))
+width_entry = Entry(volume_frame, font=("Helvetica", 20),bg='blue')
 width_entry.pack()
 
 # condition frame
 condition_options = OptionMenu(condition_frame, conditions,
                                "Pythagorean Triplet Checker", "Complimentary&Supplementary Angles").pack()
-mybutton3 = Button(condition_frame, bg='cyan', text="Select condition",
-                   command=show3).pack()
+mybutton3 = Button(condition_frame, text="Select condition",
+                   command=show3,fg='yellow',bg='red').pack()
 side1_label = Label(condition_frame, text="Enter first number").pack()
-side1_entry = Entry(condition_frame, font=("Helvetica", 20))
+side1_entry = Entry(condition_frame, font=("Helvetica", 20),bg='blue')
 side1_entry.pack()
 side2_label = Label(condition_frame, text="Enter second number").pack()
-side2_entry = Entry(condition_frame, font=("Helvetica", 20))
+side2_entry = Entry(condition_frame, font=("Helvetica", 20),bg='blue')
 side2_entry.pack()
 side3_label = Label(condition_frame, text="Enter third number").pack()
-side3_entry = Entry(condition_frame, font=("Helvetica", 20))
+side3_entry = Entry(condition_frame, font=("Helvetica", 20),bg='blue')
 side3_entry.pack()
 angle_label = Label(condition_frame, text="Enter angle in degrees").pack()
-angle_entry = Entry(condition_frame, font=("Helvetica", 20))
+angle_entry = Entry(condition_frame, font=("Helvetica", 20),bg='blue')
 angle_entry.pack()
 
 # button frame
@@ -193,11 +192,11 @@ button_frame3 = Frame(condition_frame)
 button_frame3.pack()
 
 # creating buttons
-button1 = Button(button_frame1, text="Calculate", command=area , bg='yellow')
+button1 = Button(button_frame1, text="Calculate", command=area,fg='yellow',bg='red')
 button1.grid(row=0, column=0, padx=10)
-button2 = Button(button_frame2, text="Calculate", command=volume , bg='orange')
+button2 = Button(button_frame2, text="Calculate", command=volume,fg='yellow',bg='red')
 button2.grid(row=0, column=0, padx=10)
-button3 = Button(button_frame3, text="Calculate", command=condition_check , bg='pink')
+button3 = Button(button_frame3, text="Calculate", command=condition_check,fg='yellow',bg='red')
 button3.grid(row=0, column=0, padx=10)
 
 root.mainloop()
