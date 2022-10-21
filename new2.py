30607f0f6cfabf459332eec2bbc23da9e93fde1d
Archit Anand <arcanand999@gmail.com>
2022-10-21 20:14:25 +0530

    color changed

--- new2.py
+++ new2.py
@@ -1,9 +1,6 @@
 # importing all the libraries we are making use of (tkinter and custom MyMath module
-#PES2UG21CS088
-#ARCHIT ANAND
-#COLORING
+
 from tkinter import *
-from turtle import color
 from MyMath import *
 from tkinter import ttk
 from tkinter import messagebox
@@ -140,51 +137,51 @@ def show3():
 # area frame option
 shape_options = OptionMenu(
     area_frame, shapes, "Circle", "Square", "Triangle", "Rectangle").pack()
-mybutton1 = Button(area_frame, text="Select shape", command=show1,bg="orange").pack()
+mybutton1 = Button(area_frame, text="Select shape", command=show1).pack()
 
 radius_side = Label(area_frame, text="Enter radius or side in m").pack()
 
-radius_entry = Entry(area_frame, font=("Helvetica", 20),bg="grey")
+radius_entry = Entry(area_frame, font=("Helvetica", 20))
 radius_entry.pack()
 
 height = Label(
     area_frame, text="Enter height or width in m if applicable else enter 0").pack()
-height_entry = Entry(area_frame, font=("Helvetica", 20),bg="red")
+height_entry = Entry(area_frame, font=("Helvetica", 20))
 height_entry.pack()
 
 
 # volume frame option
 volume_options = OptionMenu(
     volume_frame, solidshape, "Cone", "Sphere", "Cylinder", "Cube", "Cuboid").pack()
-mybutton2 = Button(volume_frame, text="Select shape", command=show2,bg="yellow").pack()
+mybutton2 = Button(volume_frame, text="Select shape", command=show2).pack()
 radius_side2 = Label(volume_frame, text="Enter radius or side in m").pack()
-radius_entry2 = Entry(volume_frame, font=("Helvetica", 20),bg="green")
+radius_entry2 = Entry(volume_frame, font=("Helvetica", 20))
 radius_entry2.pack()
 height2 = Label(
     volume_frame, text="Enter height in m if applicable else enter 0").pack()
-height2 = Entry(volume_frame, font=("Helvetica", 20),bg="red")
+height2 = Entry(volume_frame, font=("Helvetica", 20))
 height2.pack()
 width = Label(
     volume_frame, text="Enter breadth in m if applicable else enter 0").pack()
-width_entry = Entry(volume_frame, font=("Helvetica", 20),bg="blue")
+width_entry = Entry(volume_frame, font=("Helvetica", 20))
 width_entry.pack()
 
 # condition frame
 condition_options = OptionMenu(condition_frame, conditions,
                                "Pythagorean Triplet Checker", "Complimentary&Supplementary Angles").pack()
 mybutton3 = Button(condition_frame, text="Select condition",
-                   command=show3,bg="violet").pack()
+                   command=show3).pack()
 side1_label = Label(condition_frame, text="Enter first number").pack()
-side1_entry = Entry(condition_frame, font=("Helvetica", 20),bg="blue")
+side1_entry = Entry(condition_frame, font=("Helvetica", 20))
 side1_entry.pack()
 side2_label = Label(condition_frame, text="Enter second number").pack()
-side2_entry = Entry(condition_frame, font=("Helvetica", 20),bg="green")
+side2_entry = Entry(condition_frame, font=("Helvetica", 20))
 side2_entry.pack()
 side3_label = Label(condition_frame, text="Enter third number").pack()
-side3_entry = Entry(condition_frame, font=("Helvetica", 20),bg="red")
+side3_entry = Entry(condition_frame, font=("Helvetica", 20))
 side3_entry.pack()
 angle_label = Label(condition_frame, text="Enter angle in degrees").pack()
-angle_entry = Entry(condition_frame, font=("Helvetica", 20),bg="blue")
+angle_entry = Entry(condition_frame, font=("Helvetica", 20))
 angle_entry.pack()
 
 # button frame
@@ -196,11 +193,11 @@ button_frame3 = Frame(condition_frame)
 button_frame3.pack()
 
 # creating buttons
-button1 = Button(button_frame1, text="Calculate", command=area,bg="grey")
+button1 = Button(button_frame1, text="Calculate", command=area)
 button1.grid(row=0, column=0, padx=10)
-button2 = Button(button_frame2, text="Calculate", command=volume,bg="orange")
+button2 = Button(button_frame2, text="Calculate", command=volume)
 button2.grid(row=0, column=0, padx=10)
-button3 = Button(button_frame3, text="Calculate", command=condition_check,bg="blue")
+button3 = Button(button_frame3, text="Calculate", command=condition_check)
 button3.grid(row=0, column=0, padx=10)
 
 root.mainloop()
